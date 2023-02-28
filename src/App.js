18823670f6a4cef698a1c39402312faeadfef6c0
NishkarshRaj <nishkarshraj000@gmail.com>
2023-02-28 12:38:10 +0530

    chore: setup CRA + gitignore
    Signed-off-by: NishkarshRaj <nishkarshraj000@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Exploring Best GitHub Practices for the End-to-End Supply Chain.
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/NishkarshRaj"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          "Developed by Nish"
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
